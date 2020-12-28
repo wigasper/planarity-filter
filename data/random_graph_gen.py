e8c96a7d2349f6bc949389f99246b0b8905fde81
William Gasper <wkg@williamgasper.com>
2020-12-28 14:15:03 -0600

    random graph gen

--- data/random_graph_gen.py
+++ data/random_graph_gen.py
@@ -8,14 +8,14 @@ def main():
                 "total number of nodes in graph", required=True, type=int)
     args = parser.parse_args()
     
-    densities = [x / 100.0 for x in range(100)]
+    densities = [x / 1000.0 for x in range(1, 100)]
 
     for density in densities:
         edges = int(density * args.nodes * (args.nodes - 1) * 1/2)
 
         graph = gnm_random_graph(args.nodes, edges)
 
-        with open(f"density_trial/{int(density * 100)}_density_graph", "w") as out:
+        with open(f"density_trial/{int(density * 1000)}_density_graph", "w") as out:
 
             for edge in graph.edges:
                 out.write(" ".join([str(edge[0]), str(edge[1]), "\n"]))
