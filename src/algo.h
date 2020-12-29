6e2b532f056a1bd4edd755b4f3958d922a0f0dfd
William Gasper <wkg@williamgasper.com>
2020-12-29 09:45:48 -0600

    working on more graphlets, fixed mistake

--- src/algo.h
+++ src/algo.h
@@ -131,7 +131,7 @@ void add_houses(const node x, const adjacency_list &adj_list,
 				if (v != z && v != w) {
 				    search = nu.find(v);
 				    aux_search = aux.find(v);
-				    if (search != nu.end() && aux_search != nu.end()) {
+				    if (search != nu.end() && aux_search != aux.end()) {
 					out.push_back(x);
 					out.push_back(y);
 					out.push_back(x);
@@ -177,6 +177,63 @@ void add_houses(const node x, const adjacency_list &adj_list,
 	}
     }
 }
+
+void add_diamonds_alt(const node x, const adjacency_list &adj_list,
+	std::unordered_set<node> &nu, std::vector<node> &out, std::deque<node> &active) {
+    
+    std::vector<node> x_adjs = adj_list.at(x);
+    std::unordered_set<node> aux(x_adjs.begin(), x_adjs.end());
+     for (node y : x_adjs) {
+	auto search = nu.find(y);
+	if (search != nu.end()) {
+	    std::vector<node> y_adjs = adj_list.at(y);
+	    
+	    bool found = false;
+
+	    for (node z : y_adjs) {
+		search = nu.find(z);
+		auto aux_search = aux.find(z);
+		if (search != nu.end() && aux_search != nu.end()) {
+		    std::vector<node> z_adjs = adj_list.at(z);
+		    for (node w : z_adjs) {
+			search = nu.find(w);
+			auto aux_search = std::find(y_adjs.begin(), y_adjs.end(), w);
+			if (search != nu.end() && aux_search != y_adjs.end()) {
+			    out.push_back(x);
+			    out.push_back(y);
+			    out.push_back(x);
+			    out.push_back(z);
+			    out.push_back(y);
+			    out.push_back(z);
+			    out.push_back(x);
+			    out.push_back(w);
+			    out.push_back(z);
+			    out.push_back(w);
+
+			    active.push_front(y);
+			    active.push_front(z);
+			    active.push_front(w);
+
+			    nu.erase(y);
+			    nu.erase(z);
+			    nu.erase(w);
+
+			    aux.erase(y);
+			    aux.erase(z);
+			    aux.erase(w);
+
+			    found = true;
+
+			    break;
+			}	    
+		    } 		
+		}
+		if (found) {break;}
+	    }
+	}
+    }
+}  
+
 void add_diamonds(const node x, const adjacency_list &adj_list,
 	std::unordered_set<node> &nu, std::vector<node> &out, std::deque<node> &active) {
     
@@ -197,7 +254,7 @@ void add_diamonds(const node x, const adjacency_list &adj_list,
 		    for (node w : z_adjs) {
 			search = nu.find(w);
 			aux_search = aux.find(w);
-			if (search != nu.end() && aux_search != nu.end()) {
+			if (search != nu.end() && aux_search != aux.end()) {
 			    out.push_back(x);
 			    out.push_back(y);
 			    out.push_back(x);
@@ -246,7 +303,7 @@ void add_triangles(const node x, const adjacency_list &adj_list,
 	    for (node z : y_adjs) {
 		search = nu.find(z);
 		auto aux_search = aux.find(z);
-		if (search != nu.end() && aux_search != nu.end()) {
+		if (search != nu.end() && aux_search != aux.end()) {
 
 		    // add the edges to out, again, this is not super
 		    // clear right now and should be cleaned up. possibly
