2393f99c7ae263b7c752eae758dd2d434fa95d4b
William Gasper <wkg@williamgasper.com>
2021-01-12 09:44:46 -0600

    experiment setup

--- src/main.cpp
+++ src/main.cpp
@@ -15,8 +15,8 @@
 
 // Initializes the logger
 void log_init() {
-      std::string log_format = "[%TimeStamp%] [%Severity%] [%Message%]";
-      std::string log_path = "planarity_filter.log";
+      std::string log_format = "[%TimeStamp%] [%Message%]";
+      std::string log_path = "thread_exp.log";
       boost::log::add_file_log(log_path, boost::log::keywords::format = log_format,
                                boost::log::keywords::open_mode = std::ios_base::app);
       boost::log::add_console_log(std::cout, boost::log::keywords::format = log_format);
@@ -27,16 +27,14 @@ void log_init() {
 int main(int argc, char *argv[]) {
     log_init();
     
-    int num_threads = 1;
-    
+    size_t num_replicates = 50; 
+    size_t num_to_save = 10;
     // Get args
     namespace po = boost::program_options;
 
     po::options_description desc("Arguments");
     desc.add_options()("help,h", "display help message")
-        ("input,i", po::value<std::string>()->required(), "input file path")
-        ("output,o", po::value<std::string>()->required(), "output file path")
-	("threads,t", po::value<int>(&num_threads), "number of threads to use");
+        ("input,i", po::value<std::string>()->required(), "input file path");
 
     po::variables_map var_map;
 
@@ -62,8 +60,7 @@ int main(int argc, char *argv[]) {
     BOOST_LOG_TRIVIAL(info) << "git branch: " << GIT_BRANCH;
     BOOST_LOG_TRIVIAL(info) << "abbrev. commit hash: " << GIT_COMMIT_HASH;
     BOOST_LOG_TRIVIAL(info) << "Input: " << var_map["input"].as<std::string>();
-    BOOST_LOG_TRIVIAL(info) << "Output: " << var_map["output"].as<std::string>();
-    BOOST_LOG_TRIVIAL(info) << "Num. threads: " << num_threads;
+    BOOST_LOG_TRIVIAL(info) << "Num. replicates: " << num_replicates;
 
     BOOST_LOG_TRIVIAL(info) << "Loading input";
 
@@ -83,23 +80,28 @@ int main(int argc, char *argv[]) {
     // dedup input graph
     dedup(input_graph);
 
-    BOOST_LOG_TRIVIAL(info) << "Running algo_routine";
-    auto start = std::chrono::high_resolution_clock::now();
+    std::vector<int> thread_nums {1, 2, 4, 8, 16, 32, 64};
+
+    for (int threads : thread_nums) {
+	auto start = std::chrono::high_resolution_clock::now();
+	adjacency_list result_graph = algo_routine(input_graph, threads);
+	auto finish = std::chrono::high_resolution_clock::now();
+	std::chrono::duration<double> elapsed = finish - start;
+	BOOST_LOG_TRIVIAL(info) << "$" << threads << "," << elapsed.count() <<"$";	
+
+    }
+    BOOST_LOG_TRIVIAL(info) << "########################"
     adjacency_list result_graph = algo_routine(input_graph, num_threads);
-    auto finish = std::chrono::high_resolution_clock::now();
-    std::chrono::duration<double> elapsed = finish - start;
-    
     dedup(result_graph);
 
     if (!boyer_myrvold_test(result_graph)) {
-        BOOST_LOG_TRIVIAL(error) << "Error: the result graph is not planar";
-        exit(EXIT_FAILURE);
+	BOOST_LOG_TRIVIAL(error) << "Error: the result graph is not planar";
+	exit(EXIT_FAILURE);
     }
 
     size_t input_n_edges = num_edges(input_graph);
     size_t result_n_edges = num_edges(result_graph);
 
-    BOOST_LOG_TRIVIAL(info) << "Execution time: " << elapsed.count() << "s";
     BOOST_LOG_TRIVIAL(info) << "Initial graph - " << "nodes: " << input_graph.size()
         << " edges: " << input_n_edges;
     BOOST_LOG_TRIVIAL(info) << "Result graph - " << "nodes: " << result_graph.size()
